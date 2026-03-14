35ef26ab0f9e93c814e21902d1cf015bcad7bd65
conkhicon1369 <conkhicon1369@gmail.com>
2026-03-14 18:55:34 +0700

    1.1

--- client/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-// frontend/src/redux/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import rentalReducer from './rentalSlice';
-
-export const store = configureStore({
-  reducer: {
-    rental: rentalReducer
-  }
-});
